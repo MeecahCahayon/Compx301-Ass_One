c1c8250977ef1bc33a502b662a917b43c2b2c527
Meecah Cahayon <mc211@students.waikato.ac.nz>
2020-03-23 16:42:07 +1300

    Updating TrieNode to have phrasenum and char

--- TrieNode.java
+++ TrieNode.java
@@ -1,16 +1,39 @@
-// trie node 
-public class TrieNode 
+/* Name: Meecah Cahayon + Eunice Llobet
+ * Student ID: 1259825 + 1330233
+ */
+
+class TrieNode 
 { 
     // Alphabet size (# of symbols) 
     private final int ALPHABET_SIZE = 26;
-     
     public TrieNode[] children = new TrieNode[ALPHABET_SIZE]; 
-    public boolean isEndOfWord; 
-          
+    public boolean isEndOfWord;
+
+
+    //Creating private variables
+    private int _phraseNum;
+    private String _phraseChar;
+    
     public TrieNode() { 
         isEndOfWord = false; 
         for (int i = 0; i < ALPHABET_SIZE; i++) {
             children[i] = null; 
         }
-    } 
+    }
+
+    //Constructor
+    public TrieNode(int phraseNum, String phraseChar) {
+        _phraseNum = phraseNum;
+        _phraseChar = phraseChar;
+    }
+
+    /*Getters and Setters*/
+
+    public String getPhraseChar() {
+        return _phraseChar;
+    }
+
+    public int getPhraseNum() {
+        return _phraseNum;
+    }
 }
