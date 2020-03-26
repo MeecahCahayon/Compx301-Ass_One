2d717986e5ce0012d8c356f3871ee38affdda030
Meecah Cahayon <mc211@students.waikato.ac.nz>
2020-03-27 00:30:34 +1300

    Finished LZWencode algorithm

--- LZWencode.java
+++ LZWencode.java
@@ -1,19 +1,19 @@
 /* Name: Meecah Cahayon + Eunice Llobet
- * Student ID: 1259825 + 1330233
+ * Student ID: 1259825 + 
  */
 
 import java.util.*;
 import java.io.*;
 
-public class LZWencode {
+class MLZWencode {
 
     public static void main(String[] args) {
         
         //if there's 0 || >1 argument
-        if (args.length != 2) {
+        if (args.length != 1) {
             
             //print error
-            System.err.println("Enter valid argument: java LZWencode <textcode> <textcode>");
+            System.err.println("Enter valid argument: java LZWencode <Input text File>");
             return;
         }
 
@@ -21,40 +21,50 @@ public class LZWencode {
             
             //Initialise to read and write bytes from file
             InputStream fis = new FileInputStream(args[0]);
-            OutputStream fos = new FileOutputStream(args[1]);
+            //OutputStream fos = new FileOutputStream(args[1]);
 
+            //variables
+            Trie myTrie = new Trie();
+            TrieNode parentNode, currNode;
+            int phraseNum = 257;
+            
             //read first byte
             int intInput = fis.read();
-
-            //Creating a Trie
-            Trie trie = new Trie();
-
-            //testing if children works
-            for (int rootPhraseNum = 0; rootPhraseNum < trie._root.children.size() ; rootPhraseNum++ ) {
-            
-                System.out.println("Child " + trie._root.children.get(rootPhraseNum).getPhraseNum());
-            }
+            int tempInput = intInput;
+            parentNode = myTrie.getTrieRoot();
 
             //if not the end of the stream
             while (intInput != -1) {
 
+                currNode = myTrie.FindChar(myTrie.getTrieRoot(), intInput);
+
                 //Check if its on Trie
+                while (currNode != null) {
 
+                    parentNode = currNode;
+
+                    //Testing if output works
+                    //System.out.println("Byte " + intInput + " found. Char is " + currNode.getPhraseChar());
+
+                    //next byte and node
+                    intInput = fis.read();
+                    tempInput = intInput;
+                    currNode = myTrie.FindChar(currNode, intInput);
+                }
+                    
+                //add char to trie
+                parentNode.setChild(new TrieNode(phraseNum, (char)intInput));
+                phraseNum++;
+                
                 //write on the file second argument
+                //fos.writeInt(parentNode.getPhraseNum());
                 //Testing if output works
-                System.out.println("First output is :" + intInput);
-                fos.write(intInput);
+                //System.out.println("Byte " + intInput + " is not found. phraseNum is " + parentNode.getPhraseNum());
+                System.out.println(parentNode.getPhraseNum());
 
-                //next byte
-                intInput = fis.read();
+                intInput = tempInput;
+
             }
-
-            fos.flush();
-            fos.close();
-
-            //LZWencode e = new LZWencode();
-            //e.encode(input);
-
         }
         catch(Exception eEncode) {
 
@@ -63,96 +73,10 @@ public class LZWencode {
             return;
         }
     }
-
-    /* NOTE */
-
-    // 1.) What is the difference between BufferedInputStream to InputStream? Which is better in this program?
-
-    /* END NOTE */
-
-    /**** WE MIGHT NEED TO MOVE THIS TO MAIN ***/
-
-    /*//VARIABLES: TRIE, LIST OF INTEGERS
-    //private Trie lzwTrie;
-    private Trie dictionary;
-    private Trie key;
-    private int count;
-    private ArrayList<Integer> output;
-
-
-    public LZWencode() {
-        //lzwTrie = new Trie();
-        dictionary = new Trie();
-        key = new Trie();
-        
-        output = new ArrayList<Integer>();
-    }
-
-    public ArrayList<Integer> encode(String input) {
-
-        //GET UNIQUE CHARACTERS FROM INPUT
-        String unique_char = uniqueChar(input);
-        count = 0;
-        
-        //INSERT INTO TRIE EACH CHARACTERS 
-        //INSERT A NUMBER TO THE LIST FOR EACH INSERT
-        for(char c: unique_char.toCharArray()) {
-            //lzwTrie.insert("" + c);
-            dictionary.insert(c + Integer.toString(count));
-            output.add(output.size() + 1);
-        }
-
-        String wc = "";
-        String w = "";
-
-        //CHECK INPUT IF CURRENT CHARACTER IS IN TRIE
-        for(char c: input.toCharArray()) {
-            wc = w+c;
-
-            //CHECK IF FIRST CHARACTER IS IN TRIE
-            if(key.search(wc)) {
-                w = wc;
-            }
-            else {
-                //ADD TO TRIE
-                count++;
-                dictionary.insert(c + Integer.toString(count));
-                
-                //output.add(output.size()+1);
-            }
-        }
-
-        return output;
-    }
-*/
-
-/**** I DONT THINK WE NEED THIS ANYMORE WILL EXPLAIN WHEN WE VID CHAT ***/
-
-
-/*    //GET ALL POSSIBLE SYMBOLS FROM INPUT
-    public String uniqueChar(String input) {
-    	String unique = "";
-
-        //GO THROUGH INPUT STRING AND CHECK IF CHARACTERS REPEAT
-    	for(int i=0; i<input.length(); i++) {
-    		int count = 1;
-
-    		for(int j=i+1; j<input.length();j++) {
-                //IF THERE'S DUPLICATE
-    			if(input.charAt(i) == input.charAt(j)) {
-    				count++;
-    			}
-    		}
-            //IF THE COUNT DIDN'T INCREMENT
-            //CHAR HAS NOT YET BEEN ENCOUNTERED
-            //THEREFORE OUTPUT
-    		if(count == 1) {
-    			unique += input.charAt(i);
-    		}
-    	}
-    	return unique;
-    }
-*/
-
-
 }
+
+/* NOTE */
+
+// 1.) What is the difference between BufferedInputStream to InputStream? Which is better in this program?
+
+/* END NOTE */
