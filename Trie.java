adb933c91208367d1f77a644c0f24887113f8d21
Meecah Cahayon <mc211@students.waikato.ac.nz>
2020-03-27 00:29:52 +1300

    Update Constructor and add findChar and getTrieRoot methods

--- Trie.java
+++ Trie.java
@@ -2,9 +2,10 @@
  * Student ID: 1259825 + 1330233
  */
 
-// Java implementation of search and insert operations 
-// on Trie 
-public class Trie {
+import java.util.*;
+import java.io.*;
+
+class Trie {
 
     //creating private variables
     //temporary public (for testing the output)
@@ -15,62 +16,32 @@ public class Trie {
 
         _root = new TrieNode();
 
-        for (int rootPhraseNum = 1; rootPhraseNum < 257 ; rootPhraseNum++ ) {
+        //CREATE EXPECTED SYMBOLS FOR TRIE
+        for (int rootPhraseNum = 0; rootPhraseNum < 256 ; rootPhraseNum++ ) {
             
-            _root.setChild(new TrieNode(rootPhraseNum));
+            //CHANGE INT INTO ASCII CHARACTER THEN ADD CHILDREN
+            char rootPhraseChar = (char)rootPhraseNum;
+            _root.setChild(new TrieNode(rootPhraseNum + 1, rootPhraseChar));
         }
     }
 
-    /**** WE MIGHT NEED TO FIX SOME OF THE CODE BECAUSE OF THE CHANGE TO ARRAYLIST ***/
-/*
-    // If not present, inserts key into trie 
-    // If the key is prefix of trie node,  
-    // just marks leaf node 
-    public void insert(String key) 
-    { 
-        TrieNode node = root; 
-        int index = 0;
-        
-        //GO THROUGH STRING KEY
-        for (int i=0; i<key.length(); i++) 
-        { 
-            //GET INDEX OF CHARACTER IN ASCII
-            index = key.charAt(i) - 'a'; 
+    //Method to find char in trie
+    public TrieNode FindChar(TrieNode node, int intInput) {
 
-            //CHECK IF TRIE ALREADY HAS CHARACTER
-            if (node.children[index] == null) {
-                //MAKE NEW CHARACTER A NODE
-                node.children[index] = new TrieNode();
-                
+        TrieNode curr = node;
+
+        for (int i = 0; i < curr.getChildren().size(); i++ ) {
+            
+            if (curr.getChildren().get(i).getPhraseChar() == intInput) {
+                return curr.getChildren().get(i);
             }
-            //CHECK DOWN THE TRIE TO GET CHILDREN NODE
-            node = node.children[index]; 
         }
-        //END OF TRIE 
-        node.isEndOfWord = true; 
-    } 
-       
-    // Returns true if key presents in trie, else false 
-    public boolean search(String key) 
-    { 
-        TrieNode node = root;
-        int index; 
-        
-        for (int i=0; i<key.length(); i++) 
-        { 
-            //GET THE POSITION OF THE LETTER IN THE TRIE
-            index = key.charAt(i) - 'a'; 
-       
-            //IF KEY IS NOT IN A NODE
-            if (node.children[index] == null) 
-            {
-                return false;
-            }
-            //CHECK REST OF THE CHILDREN NODE
-            node = node.children[index]; 
-        } 
-       
-        return (node != null && node.isEndOfWord); 
+
+        //if not found
+        return null;
     }
-*/
+
+    public TrieNode getTrieRoot() {
+        return _root;
+    }
 }
