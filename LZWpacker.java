0e297531c17162c41ac40ba0c5a4094269a88c45
Meecah Cahayon <mc211@students.waikato.ac.nz>
2020-04-01 21:55:26 +1300

    Updated the packer delete redundant code

--- LZWpacker.java
+++ LZWpacker.java
@@ -8,8 +8,8 @@ import java.io.*;
 class LZWpacker {
 	
 	public static void main(String[] args) {
-		
-		//PASS IN A TEXT FILE FOR INPUT
+        
+        //PASS IN A TEXT FILE FOR INPUT
         if (args.length != 1) {
             
             //PRINT ERROR MESSAGE
@@ -19,7 +19,7 @@ class LZWpacker {
 
         try {
 
-        	//READ AS STREAM OF BYTES FROM FILE
+            //READ AS STREAM OF BYTES FROM FILE
             InputStream file = new FileInputStream(args[0]);
             BufferedReader reader = new BufferedReader(new InputStreamReader(file));
 
@@ -28,63 +28,64 @@ class LZWpacker {
             int maxBit = 32;
             int maskOutput = 0xff000000;
             int packer = 0;
-            int trackerCount = 0;
+            int bitTracker = 0;
             int output = 0;
-            int bitCount = (int)(Math.ceil(Math.log(maxPhraseNum) / Math.log(2)));
-			            
+            int bitCount;
+                        
             //READS A LINE
             String line = reader.readLine();
 
             //WHILE NOT END OF THE FILE
             while (line != null) {
-            	
-            	//CONVERTS LINE INTO INT
-            	int inputNum = Integer.parseInt(line);
+                
+                //CONVERTS LINE INTO INT
+                int inputNum = Integer.parseInt(line);
 
-            	//Get new bitCount
-            	bitCount = (int)(Math.ceil(Math.log(maxPhraseNum) / Math.log(2)));
+                //Get new bitCount
+                bitCount = (int)(Math.ceil(Math.log(maxPhraseNum) / Math.log(2)));
 
-            	//shift phrase number by (maxBit - trackerCount - bitCount) to the left
-            	int newPhraseNum = inputNum << (maxBit - trackerCount - bitCount);
+                //shift phrase number by (maxBit - bitTracker - bitCount) to the left
+                int newPhraseNum = inputNum << (maxBit - bitTracker - bitCount);
+                
+                //add new phrase number to packer
+                packer = packer | newPhraseNum;
 
-            	//add new phrase number to packer
-            	packer = packer | newPhraseNum;
+                //track how many bits in the packer
+                bitTracker += bitCount;
 
-            	//
-            	trackerCount += bitCount;
+                //mask the packer according to bitTracker and bitCount
+                packer = doMasking(packer, bitTracker);
+                maxPhraseNum++;
 
-            	//mask the packer according to trackerCount and bitCount
-            	packer = doMasking((trackerCount + bitCount), packer);
-            	maxPhraseNum++;
-        
-            	//while there's 8 or more bits
-            	while (trackerCount > 7) {
+                //shift the packer by 8 to the left and outpit it
+                output = packer & maskOutput;
+                output = output >>> 24;
+                byte outputByte = (byte)output;
 
-            		//shift the packer by 8 to the left and outpit it
-            		output = packer & maskOutput;
-            		output = output >>> 24;
-            		byte outputByte = (byte)output;
-            		packer = packer << 8;
-            		trackerCount -= 8;
+                //remove output from packer
+                packer = packer << 8;
+                bitTracker -= 8;
 
-            		System.out.write(outputByte);
-            	}
+                //output
+                System.out.write(outputByte);
 
-            	//NEXT LINE
+                //NEXT LINE
                 line = reader.readLine();
             }
 
-            if (trackerCount != 0) {
-            	
-            	//shift the packer by 8 to the left and outpit it
-        		output = packer & maskOutput;
+            //if there's still bits in the packer
+            while (bitTracker >= 0) {
+                
+                //shift the packer by 8 to the left and outpit it
+                output = packer & maskOutput;
 
-        		// System.out.println("byte 1 " + output);
-        		output = output >>> 24;
-        		byte outputByte = (byte)output;
-        		packer = packer << 8;
+                // System.out.println("byte 1 " + output);
+                output = output >>> 24;
+                byte outputByte = (byte)output;
+                packer = packer << 8;
+                bitTracker -= 8;
 
-        		System.out.write(outputByte);
+                System.out.write(outputByte);
             }
 
             System.out.flush();
@@ -92,34 +93,34 @@ class LZWpacker {
         }
         catch (Exception ePacker) {
 
-        	//PRINT ERROR
+            //PRINT ERROR
             System.err.println("Error: " + ePacker);
             return;
         }
-	}
+    }
 
-	static int doMasking(int bitCount, int packer) {
+    static int doMasking(int packer, int bitCount) {
 
-		String strMask = "0";
-		int intMask;
-		int count = 0;
+        String strMask = "";
+        int intMask;
+        int count = 0;
 
-		for (int i = 0; i < bitCount; i++ ) {
-			
-			strMask += "1";
-			count++;
-		}
-	
-		while (count < 31) {
-			
-			strMask += "0";
-			count++;
-		}
+        for (int i = 0; i < bitCount; i++ ) {
+            
+            strMask += "1";
+            count++;
+        }
+    
+        while (count < 31) {
+            
+            strMask += "0";
+            count++;
+        }
 
-    	intMask = Integer.parseInt(strMask,2);
-    	intMask = intMask << 1;
-    	packer = packer & intMask;
+        intMask = Integer.parseInt(strMask,2);
+        intMask = intMask << 1;
+        packer = packer & intMask;
 
-		return packer;
-	}
+        return packer;
+    }
 }
